104d28d58cbb438e642dcca4f73297bfb4278869
kirancr9 <subs.kiran@gmail.com>
2018-10-15 16:33:18 +0530

    das

--- first/Routes/books.js
+++ first/Routes/books.js
@@ -16,12 +16,76 @@ router.get("/View",function(req,res){
             else
             {
                 res.render("ViewBooks",{"data":result});
-                console.log(result);
+            
             }
         })
 
     });
 })
+
+router.get("/New",function(req,res){
+
+    res.render("NewBook")
+})
+
+router.post("/New",function(req,res){
+    mongoclient.connect(url,function(err,database){
+        var db = database.db("sampledb");
+        var collection = db.collection('Books');
+        var BookData = {"BookID":req.body.bid, "BookName":req.body.bname, "Author":req.body.bauth, "Price":req.body.bpr, "Publisher":req.body.bpub}
+        collection.insert(BookData,function(err,database){
+            if(err){
+                console.log("error");
+
+            }
+            else{
+                console.log("New Book Added");
+                res.redirect("/");
+            }
+        })
+    })
+})
+
+
+
+router.get("/Del", function (req, res) {
+
+    mongoclient.connect(url, function (err, database) {
+        var db = database.db("sampledb");
+        var collection = db.collection("Books");
+        collection.find({},{"BookID":1,"_id":0}).toArray(function (err, result) {
+
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(result);
+                
+                res.render("DelBook",{"data":result});
+            }
+        })
+
+    });
+})
+
+router.post("/Deleted", function (req, res) {
+
+    mongoclient.connect(url, function (err, database) {
+        var db = database.db("sampledb");
+        var collection = db.collection("Books");
+        var bookID= req.body.sel;
+        console.log(bookID);
+        collection.remove({"BookID":bookID},function(err,result){
+
+            console.log("Book deleted")
+            res.redirect("/");
+        })
+
+
+
+    });
+})
+
 
 module.exports = router;
 
--- first/app.js
+++ first/app.js
@@ -1,6 +1,8 @@
-var express = require('express')
-var app = express()
-var books = require("./Routes/books")
+var express = require('express');
+var app = express();
+var books = require("./Routes/books");
+var bodyparser=require('body-parser');
+app.use(bodyparser.urlencoded({extended:true}))
 
 app.set('view engine','ejs')
 
